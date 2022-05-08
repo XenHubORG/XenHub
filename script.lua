e865378872c3b9470c341c372a05952100921a42
Xenvant <horseboxofficialyt@gmail.com>
2022-05-08 18:17:55 +1000

    [XenHub] - Version 1.2a
    - Added more Place checks
    - Made script hop faster and easier

--- script.lua
+++ script.lua
@@ -1,3 +1,6 @@
+-- Services
+local TeleportService = game:GetService("TeleportService")
+
 -- Constants
 local player = game.Players.LocalPlayer
 
@@ -8,16 +11,24 @@ local placeIds = {
     ["Demonfall-Server"] = 5094651510
 }
 
+-- Wait till game loaded
 repeat wait() until game:IsLoaded()
 
+-- Handle placeid
 local placeId = game.PlaceId
 if placeId == placeIds["Greenville"] then
     
     --loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenvant/XenHub/main/Games/GV.lua"))()
     player:Kick("[XenHub] - Greenville script disabled!")
 
-elseif placeId == placeIds["Demonfall-Main"] or placeId == placeIds["Demonfall-Server"] then
+elseif placeId == placeIds["Demonfall-Main"] then
     
+    -- Teleport to server
+    TeleportService:Teleport(5094651510, player)
+
+elseif placeId == placeIds["Demonfall-Server"] then
+    
+    -- Load demonfall script
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenvant/XenHub/main/Games/Demonfall.lua"))()
 
 end
