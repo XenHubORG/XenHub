d7a64971daf739541ac0d417170944295a7a1218
Xenvant <horseboxofficialyt@gmail.com>
2022-05-08 18:07:05 +1000

    [XenHub] - Version 1.2
    - Using https://luauth.xyz whitelist and security for the script
    - Added Demonfall
    - Disabled Greenville (rewrite)

--- script.lua
+++ script.lua
@@ -3,13 +3,21 @@ local player = game.Players.LocalPlayer
 
 -- Configuration
 local placeIds = {
-    ["Greenville"] = 891852901
+    ["Greenville"] = 891852901,
+    ["Demonfall-Main"] = 4855457388,
+    ["Demonfall-Server"] = 5094651510
 }
 
 repeat wait() until game:IsLoaded()
 
-if game.PlaceId == placeIds["Greenville"] then
+local placeId = game.PlaceId
+if placeId == placeIds["Greenville"] then
     
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenvant/XenHub/main/Games/GV.lua"))()
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenvant/XenHub/main/Games/GV.lua"))()
+    player:Kick("[XenHub] - Greenville script disabled!")
+
+elseif placeId == placeIds["Demonfall-Main"] or placeId == placeIds["Demonfall-Server"] then
+    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenvant/XenHub/main/Games/Demonfall.lua"))()
 
 end
